e92587ee6573ea5cd367b8e24de8af2ca1eb1f09
Rashmil333 <68959771+Rashmil333@users.noreply.github.com>
2021-08-04 19:09:36 +0530

    commit

---
+++ Testenvelope.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Testenvelope=()=>{
+	return(<>
+		<div style={{margin:'25%',marginLeft:'39%'}}>
+			<a id="en1" href="#">Rashmil</a>
+			<p class='p1'>hello from the envelope side.</p>
+
+
+		</div>
+
+	
+		
+
+	</>)
+}
+
+export default Testenvelope;
